93cb0c565b6a2c16016ebd2f60013392e4cb2828
28Wahab <28muhammadabdulwahab@gmail.com>
2025-07-20 12:02:18 +0700

    Update project setelah perubahan terbaru

--- src/index.js
+++ src/index.js
@@ -17,6 +17,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
 
+// core styles
+import "./scss/volt.scss";
+
 // vendor styles
 import "react-datetime/css/react-datetime.css";
 
