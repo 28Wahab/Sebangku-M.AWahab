02d1b4d2f1f77a1f6683b0aa3b7216ac40c79ef0
28Wahab <28muhammadabdulwahab@gmail.com>
2025-07-20 11:58:31 +0700

    Update project setelah perubahan terbaru

--- src/index.js
+++ src/index.js
@@ -13,12 +13,9 @@
 
 // * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. Please contact us to request a removal.
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
-
-// core styles
-import "./scss/volt.scss";
 
 // vendor styles
 import "react-datetime/css/react-datetime.css";
